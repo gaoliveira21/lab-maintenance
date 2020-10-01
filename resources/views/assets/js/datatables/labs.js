1fcefe67833f4a0d3c8c386f548d444fcb199378
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-10-01 15:08:13 -0300

    fix remove button

--- resources/views/assets/js/datatables/labs.js
+++ resources/views/assets/js/datatables/labs.js
@@ -1,21 +1,25 @@
 $(document).ready(function () {
     const id = 'labs-table';
-    const table = initDataTables(id);
+    const table = initDataTables(id, {
+        drawCallback: () => del()
+    });
 
-    $('button.remove').click(async function () {
-        const trigger = $(this);
-        const url = trigger.data('action');
-        const result = await SweetAlert.confirmAlert();
+    function del() {
+        $('button.remove').on('click', async function () {
+            const trigger = $(this);
+            const url = trigger.data('action');
+            const result = await SweetAlert.confirmAlert();
 
-        if (result.value) {
-            try {
-                await axios.delete(url);
+            if (result.value) {
+                try {
+                    await axios.delete(url);
 
-                removeRow(table, trigger);
-                SweetAlert.success();
-            } catch (error) {
-                SweetAlert.fails();
+                    removeRow(table, trigger);
+                    SweetAlert.success();
+                } catch (error) {
+                    SweetAlert.fails();
+                }
             }
-        }
-    })
+        })
+    }
 });
